ab038e14dfb1656ceb9cf887af1ff294b214cbea
chimey <selamet.antsoftmedia@gmail.com>
2023-06-15 22:07:31 +0700

    fix

--- app/Services/Video/VideoStraem.ts
+++ app/Services/Video/VideoStraem.ts
@@ -45,7 +45,7 @@ export class VideoStream extends EventEmitter {
     public start(): void {
       try {
 
-        this.wsServer = new Server<WebSocketMeta>({ port: this.options?.wsPort || 9999, host: '0.0.0.0' });
+        this.wsServer = new Server<WebSocketMeta>({ port: this.options?.wsPort || 9999, host: '0:0:0:0' });
 
         this.wsServer.on('connection', (socket, request) => {
             if (!request.url) { return; }
